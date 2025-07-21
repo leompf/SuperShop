69c1dfd65e12437ebe8dde8daeb8c148d48543a2
leompf <lmfraqueiro@gmail.com>
2025-07-21 18:00:19 +0100

    Generic Repository

--- SuperShop/Startup.cs
+++ SuperShop/Startup.cs
@@ -27,7 +27,7 @@ namespace SuperShop
             );
 
             services.AddTransient<SeedDB>();
-            services.AddScoped<IRepository, Repository>();
+            services.AddScoped<IProductRepository, ProductRepository>();
 
             services.AddControllersWithViews();
         }
